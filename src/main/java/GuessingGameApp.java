8778f7d5e79ac6a1b2787aae4190be3055cb5237
Connor <connor@chamfam.net>
2021-01-22 16:11:57 -0500

    part 3 done

--- src/main/java/GuessingGameApp.java
+++ src/main/java/GuessingGameApp.java
@@ -1,3 +1,5 @@
+
+
 import java.util.Scanner;
 
 public class GuessingGameApp {
@@ -5,13 +7,18 @@ public class GuessingGameApp {
     public static void main(String[] args) {
         Scanner keyboard = new Scanner(System.in);
         int guess = keyboard.nextInt();
-        if (guess == 7) {
-            System.out.println("You won the Guessing Game!");
-        } else if( guess == 0) {
-            System.out.println("Oh you don't know how to play? Guess a number 1-10.");
-        } else {
-            System.out.println("You are bad a guessing. You lost.");
+        boolean win = false;
+        while (!win) {
+            if (guess == 7) {
+                System.out.println("You won the Guessing Game!");
+                win = true;
+            } else if( guess == 0) {
+                System.out.println("Oh you don't know how to play? Guess a number 1-10.");
+            } else {
+                System.out.println("You are bad a guessing. You lost.");
+            }
         }
+
     }
 
 }
