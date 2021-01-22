c2c488d4c626955b7a838cf2c49236119d1932bc
Connor <connor@chamfam.net>
2021-01-22 09:48:54 -0500

    7 is the right guess.

--- src/main/java/GuessingGameApp.java
+++ src/main/java/GuessingGameApp.java
@@ -1,7 +1,15 @@
+import java.util.Scanner;
+
 public class GuessingGameApp {
 
     public static void main(String[] args) {
-
+        Scanner keyboard = new Scanner(System.in);
+        int guess = keyboard.nextInt();
+        if (guess == 7) {
+            System.out.println("You won the Guessing Game!");
+        } else {
+            System.out.println("You are bad a guessing. You lost.");
+        }
     }
 
 }
