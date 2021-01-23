450ece0e3f296816f006bfd35cd1be8e58d79cc8
Connor <connor@chamfam.net>
2021-01-23 09:23:57 -0500

    reread prompt and added missing detail of /'one more time/'

--- src/main/java/GuessingGameApp.java
+++ src/main/java/GuessingGameApp.java
@@ -8,17 +8,26 @@ public class GuessingGameApp {
         Scanner keyboard = new Scanner(System.in);
         int guess = keyboard.nextInt();
         boolean win = false;
+        int guessCount = 0;
         while (!win) {
+            if (guessCount>1) {
+                System.out.println("Game over!");
+                break;
+            }
             if (guess == 7) {
                 System.out.println("You won the Guessing Game!");
                 win = true;
             } else if( guess == 0) {
                 System.out.println("Oh you don't know how to play? Guess a number 1-10.");
+                guessCount--;
                 guess = keyboard.nextInt();
+                // There is a error here, if you guess wrong first and then guess 0 twice,
+                // you can get 2 more guesses.  Probably alternate between guess and 0 forever.
             } else {
-                System.out.println("You are bad a guessing. You lost.");
+                System.out.println("You are bad a guessing. Try again.");
                 guess = keyboard.nextInt();
             }
+            guessCount++;
         }
 
     }
