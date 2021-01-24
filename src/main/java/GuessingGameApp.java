138f66b86c238e9d660d93ae067d840d766d4776
Connor <connor@chamfam.net>
2021-01-24 10:10:48 -0500

    First time with connection, 3 steps done with if else, changing to switch case

--- src/main/java/GuessingGameApp.java
+++ src/main/java/GuessingGameApp.java
@@ -9,26 +9,30 @@ public class GuessingGameApp {
         int guess = keyboard.nextInt();
         boolean win = false;
         int guessCount = 0;
-        while (!win) {
-            if (guessCount>1) {
-                System.out.println("Game over!");
-                break;
-            }
-            if (guess == 7) {
-                System.out.println("You won the Guessing Game!");
-                win = true;
-            } else if( guess == 0) {
-                System.out.println("Oh you don't know how to play? Guess a number 1-10.");
-                guessCount--;
-                guess = keyboard.nextInt();
-                // There is a error here, if you guess wrong first and then guess 0 twice,
-                // you can get 2 more guesses.  Probably alternate between guess and 0 forever.
-            } else {
-                System.out.println("You are bad a guessing. Try again.");
-                guess = keyboard.nextInt();
-            }
-            guessCount++;
+        int
+//        while (!win) {
+//            if (guessCount>1) {
+//                System.out.println("Game over!");
+//                break;
+//            }
+//            if (guess == 7) {
+//                System.out.println("You won the Guessing Game!");
+//                win = true;
+//            } else if( guess == 0) {
+//                System.out.println("Oh you don't know how to play? Guess a number 1-10.");
+//                guessCount--;
+//                guess = keyboard.nextInt();
+//                 There is a error here, if you guess wrong first and then guess 0 twice,
+//                 you can get 2 more guesses.  Probably alternate between guess and 0 forever.
+//            } else {
+//                System.out.println("You are bad a guessing. Try again.");
+//                guess = keyboard.nextInt();
+//            }
+//            guessCount++;
+        switch (guess) {
+            case
         }
+
 
     }
 
