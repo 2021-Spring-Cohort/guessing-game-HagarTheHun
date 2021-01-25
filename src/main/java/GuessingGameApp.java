361703dd08703013929b2dc91688f4bd57c11cad
Connor <connor@chamfam.net>
2021-01-24 20:49:51 -0500

    Code complete. Test code removed.

--- src/main/java/GuessingGameApp.java
+++ src/main/java/GuessingGameApp.java
@@ -7,7 +7,7 @@ public class GuessingGameApp {
 
     public static void main(String[] args) {
         Scanner keyboard = new Scanner(System.in);
-        int guess = -2; //= keyboard.nextInt();
+        int guess = -2;
         boolean win = false;
         int guessCount = 0;
         Random numberPicker =  new Random();
@@ -16,25 +16,7 @@ public class GuessingGameApp {
             answer = numberPicker.nextInt();
         }
 
-//        while (!win) {
-//            if (guessCount>1) {
-//                System.out.println("Game over!");
-//                break;
-//            }
-//            if (guess == 7) {
-//                System.out.println("You won the Guessing Game!");
-//                win = true;
-//            } else if( guess == 0) {
-//                System.out.println("Oh you don't know how to play? Guess a number 1-10.");
-//                guessCount--;
-//                guess = keyboard.nextInt();
-//                 There is a error here, if you guess wrong first and then guess 0 twice,
-//                 you can get 2 more guesses.  Probably alternate between guess and 0 forever.
-//            } else {
-//                System.out.println("You are bad a guessing. Try again.");
-//                guess = keyboard.nextInt();
-//            }
-//            guessCount++;
+
         while (!win && guessCount<3) {
             guess = keyboard.nextInt();
             if (guess == answer) {
@@ -42,13 +24,8 @@ public class GuessingGameApp {
                 System.out.println("You won the Guessing Game!");
             }
             switch (guess) {
-//                case (answer):
-//                    win = true; // you got the right answer
-//                    System.out.println("You won the Guessing Game!");
-//                    break;
                 case 0:
                     System.out.println("Oh you don't know how to play? Guess an int.");
-                    //guess = keyboard.nextInt();
                     break;
                 case -1:
                      guessCount = 5; //quit program
@@ -60,7 +37,6 @@ public class GuessingGameApp {
                     } else {
                     System.out.println("The answer is a little lower!");
                 }
-                    //guess = keyboard.nextInt();
                 guessCount++;
             }
         }
